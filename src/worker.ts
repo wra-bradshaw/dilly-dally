4f099c9904e9ff034e4afe7ef9e16149295923dd
will <will.bradshaw50@gmail.com>
2026-09-23 18:57:24 +1000

    feat(server): frame and sniffing headers on HTML shell

--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,30 @@
 import handler from "@tanstack/react-start/server-entry";
 import { drizzle } from "drizzle-orm/d1";
+import { documentSecurityHeaders } from "./lib/api-errors";
 import { purgeExpired } from "./lib/db";
 import { RateLimiter } from "./lib/rate-limiter-do";
 import * as schema from "./lib/schema";
 
 export { RateLimiter };
 
+function withDocumentHeaders(res: Response): Response {
+	const contentType = res.headers.get("content-type") ?? "";
+	if (!contentType.includes("text/html")) return res;
+	const headers = new Headers(res.headers);
+	for (const [key, value] of Object.entries(documentSecurityHeaders())) {
+		headers.set(key, value);
+	}
+	return new Response(res.body, {
+		headers,
+		status: res.status,
+		statusText: res.statusText,
+	});
+}
+
 export default {
-	fetch: handler.fetch,
+	async fetch(...args: Parameters<typeof handler.fetch>): Promise<Response> {
+		return withDocumentHeaders(await handler.fetch(...args));
+	},
 	async scheduled(
 		_controller: ScheduledController,
 		env: Env,
